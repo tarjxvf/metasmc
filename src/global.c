87b99020ce5538c6e8d1aa99b94c0b291f4cdc78
tarjxvf <fjliao@gmail.com>
2018-06-11 03:19:34 +0800

    Further remove unnecessary initialization.

--- src/global.c
+++ src/global.c
@@ -33,7 +33,7 @@ struct event *alloc_event(struct config *cfg, int type, double t)
 	ev->t = t;
 	ev->dn = (int *)((char *)l + evsize[type]);
 	ev->sumdn = (int *)((char *)l + evsize[type] + sizeof(int) * npop_all);
-	memset(ev->dn, 0, sizeof(int) * 2 * npop_all);
+//	memset(ev->dn, 0, sizeof(int) * 2 * npop_all);
 	dn_clear(npop_all, ev->dn);
 
 #ifdef DEBUG
