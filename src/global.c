0fe5f9f05939d9d3b6de16c4d4c5e0b1d5c802d6
tarjxvf <fjliao@gmail.com>
2019-01-18 20:25:18 +0800

    Update read header.

--- src/global.c
+++ src/global.c
@@ -420,7 +420,7 @@ void print_fragment(FILE *outfp, int chrnum, int fgstart, int fgid, struct fginf
 #endif
 	for(i = 0; i < fgi->nread; i++){
 		rd = &rdset[i];
-		fprintf(outfp, ">%d:%d %d %d %d-%d %d-%d\n", fgid, i + 1, fgi->pop, chrnum, fgstart, fgi->end, rd->start, rd->end);
+		fprintf(outfp, ">%d:%d:%d:%d:%d-%d:%d-%d\n", fgid, i + 1, fgi->pop, chrnum, fgstart, fgi->end, rd->start, rd->end);
 		fprintf(outfp, "%s\n", rd->seq);
 	}
 #ifdef DEBUG
